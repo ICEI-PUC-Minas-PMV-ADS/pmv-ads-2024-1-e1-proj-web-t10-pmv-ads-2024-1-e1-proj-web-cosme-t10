590fadfe0df4d5aab929b4f31ff8c185ed150a34
Mauricio Honorato <honorato_mauricio@hotmail.com>
2024-05-17 01:12:08 -0300

    Define estado inicial do arquivo de script com instruções sobre as tarefas a serem feitas

--- codigo-fonte/script.js
+++ codigo-fonte/script.js
@@ -1,1 +1,42 @@
+//TAREFA 1:
+//Modelagem de dados (definir valores/propriedades dos ingredientes químicos).
+//Exemplo:
+// let ingredienteQuimico = {
+//     nome: "Cyclomethicone",
+//     formulaQuimica: "C10H30O5SI5",
+//     descricao: "É um líquido incolor e inodoro que é ligeiramente volátil.",
+//     efeitosAdversos: "Irritação na pele e nos olhos."
+//     fontes: "Johnson Jr, Wilbur, et al. Safety assessment of cyclomet..."
+// }
+
+//TAREFA 1.1:
+//Criar lista de elementos baseado na modelagem de dados que definirmos
+
+//TAREFA 1.2:
+//Salvar a lista pro armazenamento local.
+//Referir se ao exemplo de CRUD mostrado na etapa do projeto pelo professor Kutova nos vídeo-aulas de JS na página inicial da etapa do projeto
+// ou professor Rommel disponivel nesse link:
+//https://replit.com/@rommelpuc/Cadastro-de-Contatos-localStorage#app.js
+
+//TAREFA 1.3a:
+//Implementar método/função responsável pela busca de ingrediente(s) na lista salva nos dados locais.
+//Busca será feita pelo(s) nome(s) e deve existir a lógica que possibilita a busca de um ou mais ingredientes.
+
+//TAREFA 1.3b:
+//Implementar funcionalidade de sugestão de pesquisa incremental.
+
+//TAREFA 1.4:
+//A partir do primeiro elemento encontrado, criar os elementos necessários juntamente do reposicionamento dos elementos
+//ja existentes a fim de transformar o visual da página index.html no da página de resultado.html (sem que exista a mudan
+//ça de páginas).
+
+//TAREFA 1.5:
+//Com a lista de ingredientes retornada pela busca e com o primeiro card já criado, implementar paginação no card.
+//Recomendação: salvar página atual em uma variável própria de "contador"; contador esse que aumenta (se não for a última página
+//ou diminui (se não for a primeira página) quando navegado pelas setinhas, e serve de índice em relação a lista de ingredientes
+//retornada na busca. Então os valores do card (nome, descrição, etc) são alterados de acordo com qual "página" (ingrediente
+//em que o card está mostrando num determinado momento).
+
+//TAREFA 2:
+//Implementar a função de modo noturno.
 
